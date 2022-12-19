a7dc055f7c6590136ee4f03528183f2860f21810
ANDY-NAIDU <119730405+ANDY-NAIDU@users.noreply.github.com>
2022-12-19 22:19:47 +0530

    #include<stdio.h> int main() {     float a,b;     scanf("%f %f",&a, &b);     if((b*30)<a)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Expenditure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    scanf("%f %f",&a, &b);
+    if((b*30)<a)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
