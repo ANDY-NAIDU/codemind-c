9fa6a394d821504b3bf6a471e3a562929300b65b
ANDY-NAIDU <119730405+ANDY-NAIDU@users.noreply.github.com>
2023-01-05 12:40:13 +0530

    #include<stdio.h> int main() {  int n,sum=0,r,n1;  scanf("%d",&n1); n=n1;  while(n)  {  r=n%10;  sum=(sum*10)+r;  n/=10;  }  if(sum==n1)  printf("Palindrome");  else  printf("Not Palindrome"); }

---
+++ Palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int n,sum=0,r,n1;
+ scanf("%d",&n1); n=n1;
+ while(n)
+ {
+ r=n%10;
+ sum=(sum*10)+r;
+ n/=10;
+ }
+ if(sum==n1)
+ printf("Palindrome");
+ else
+ printf("Not Palindrome");
+} 
+ 
