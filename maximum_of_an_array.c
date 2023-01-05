5437441cdcf21a8b270e4ef1af36a5c925f03096
ANDY-NAIDU <119730405+ANDY-NAIDU@users.noreply.github.com>
2023-01-05 12:51:25 +0530

    #include<stdio.h> int main() {  int i,n,max;  scanf("%d",&n);  int x[n];  for (i=0;i<n;i++)  scanf("%d",&x[i]);  max=x[0];  for(i=0;i<n;i++)  {  if (x[i]>max)  max=x[i];
     }
     printf("%d",max);
    }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int i,n,max;
+ scanf("%d",&n);
+ int x[n];
+ for (i=0;i<n;i++)
+ scanf("%d",&x[i]);
+ max=x[0];
+ for(i=0;i<n;i++)
+ {
+ if (x[i]>max)
+ max=x[i];
+ 
+ }
+ printf("%d",max);
+}
