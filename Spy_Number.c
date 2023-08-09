bcbd70dce909a79aacf5ba99136770de72c291cf
ANDY-NAIDU <119730405+ANDY-NAIDU@users.noreply.github.com>
2023-08-10 00:28:49 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int temp=0,b=0,c=1;     while(a>0){         b=b+a%10;         c=c*(a%10);         temp=temp+a%10;         a=a/10;         temp=temp*10;     }     if(b==c){         printf("Spy Number");     }else{         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,r,sum=0,pdt=1;
-    scanf("%d",&n);
-    while(n!=0)
-    {
-        i=n%10;
-        n=n/10;
-        sum+=i;
-        pdt=pdt*i;
-        
+int main(){
+    int a;
+    scanf("%d",&a);
+    int temp=0,b=0,c=1;
+    while(a>0){
+        b=b+a%10;
+        c=c*(a%10);
+        temp=temp+a%10;
+        a=a/10;
+        temp=temp*10;
     }
-    if(sum==pdt)
-    {
+    if(b==c){
         printf("Spy Number");
+    }else{
+        printf("Not Spy Number");
     }
-    else printf("Not Spy Number");
 }
